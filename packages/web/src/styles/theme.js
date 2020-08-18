b879ad3cf36953af1072f968ddd2ea3e5b6afa65
Logqwerty <inm2790@gmail.com>
2020-08-18 22:21:03 +0900

    [Refactor] 프로젝트 전반에 사용할 theme 객체 정의
    App 컴포넌트에 하드코딩된 theme 객체를 따로 빼내어 모듈화.

---
+++ packages/web/src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  palette: {
+    primary: ['#B3DBfB', '#7AC0F8', '#42A5F5', '#2196F3', '#1E88E5'],
+    danger: ['#F8BAB9', '#F38684', '#EF5350', '#F44336', '#E53935'],
+    black: ['#000000'],
+    white: ['#FFFFFF'],
+    grayscale: [
+      '#FAFAFA',
+      '#F5F5F5',
+      '#EEEEEE',
+      '#E0E0E0',
+      '#BDBDBD',
+      '#9E9E9E',
+      '#757575',
+      '#616161',
+      '#424242',
+      '#212121',
+    ],
+  },
+};
