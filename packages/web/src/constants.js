b034b98d52a493feacaf1f56efb3fa23a4266768
Logqwerty <inm2790@gmail.com>
2020-08-20 01:01:20 +0900

    [Refactor] Icon 관련 상수를 constatns 모듈로 이동
    프로젝트에서 사용하는 상수를 src/constants 모듈에서만 관리하여, 다른 컴포넌
    트나 모듈에서 필요한 상수를 쉽게 접근할 수 있도록 함.

--- packages/web/src/constants.js
+++ packages/web/src/constants.js
@@ -1,3 +1,76 @@
 export const DEFAULT_UNIT = 3;
 export const DEFAULT_FONT_SIZE = 14;
 export const DEFAULT_FONT_WEIGHT = 600;
+
+export const ICON_IMAGE_LENGTH = 1000;
+export const ICON_LENGTH = 125;
+export const ICON_LOGO_WIDTH = 1000;
+export const ICON_LOGO_HEIGHT = 158;
+
+export const ICON_MAX_RATIO = 10;
+
+export const ICON_TYPES = Object.freeze({
+  logo: 'logo',
+  emptyHeart: 'emptyHeart',
+  fullHeart: 'fullHeart',
+  newPost: 'newPost',
+  directMessage: 'directMessage',
+  search: 'search',
+  comment: 'comment',
+  favicon: 'favicon',
+  ellipsis: 'ellipsis',
+  clear: 'clear',
+  logout: 'logout',
+  hashtag: 'hashtag',
+});
+
+export const ICON_POSITIONS = Object.freeze({
+  logo: {
+    x: 0,
+    y: 0,
+  },
+  emptyHeart: {
+    x: -130,
+    y: -246,
+  },
+  fullHeart: {
+    x: -130,
+    y: -376,
+  },
+  newPost: {
+    x: -260,
+    y: -248,
+  },
+  directMessage: {
+    x: -130,
+    y: -504,
+  },
+  search: {
+    x: -260,
+    y: -631,
+  },
+  comment: {
+    x: -520,
+    y: -248,
+  },
+  favicon: {
+    x: 0,
+    y: -505,
+  },
+  ellipsis: {
+    x: -261,
+    y: -504,
+  },
+  clear: {
+    x: -390,
+    y: -631,
+  },
+  logout: {
+    x: 0,
+    y: -248,
+  },
+  hashtag: {
+    x: -130,
+    y: -634,
+  },
+});
