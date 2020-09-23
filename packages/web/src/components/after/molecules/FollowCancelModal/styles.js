071f23f4c90102816b33cbccc68168cc550254d9
Logqwerty <inm2790@gmail.com>
2020-09-24 00:01:43 +0900

    [Feat] FollowCancelModal 컴포넌트 구현 및 스토리작성
    `팔로잉`버튼을 눌렀을 때, 팔로우를 정말 취소할 것인지 사용자에게 확인하는
    모달을 구현.

---
+++ packages/web/src/components/after/molecules/FollowCancelModal/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { palette } from 'styled-tools';
+
+import { Image, Flex, Modal, ModalMenu } from '@atoms';
+import { FLEX_ALIGN, FLEX_DIRECTION } from '@const';
+
+export const StyledModal = styled(Modal)`
+  width: 400px;
+`;
+
+export const ProfileImage = styled(Image).attrs(() => ({
+  round: true,
+}))`
+  width: 90px;
+  height: 90px;
+  margin: 32px 32px 16px;
+`;
+
+export const CancelMessage = styled.span`
+  font-size: 0.85rem;
+  margin: 16px 16px 32px;
+`;
+
+export const ModalTopFlex = styled(Flex).attrs(() => ({
+  direction: FLEX_DIRECTION.col,
+  align: FLEX_ALIGN.center,
+}))`
+  width: 100%;
+  border-bottom: 1px solid ${palette('border')};
+`;
+
+export const FollowCancelMenu = styled(ModalMenu).attrs(() => ({
+  danger: true,
+}))`
+  font-weight: bold;
+`;
