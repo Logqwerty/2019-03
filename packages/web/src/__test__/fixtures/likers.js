93e747dbd8eef8eb0bd82929620195e613edc4a1
Logqwerty <inm2790@gmail.com>
2020-09-30 22:43:25 +0900

    [Feat] 좋아요 누른 유저들에 대한 fixture 추가
    스토리북 작성 시 사용하는 데이터의 중복을 제거하기 위해 fixture를 추가.

---
+++ packages/web/src/__test__/fixtures/likers.js
@@ -0,0 +1,69 @@
+import { FOLLOW_STATUS } from '@const';
+
+const likers = [
+  {
+    username: 'liker1',
+    profileImage: null,
+    followStatus: null,
+    myId: '1',
+    userId: '2',
+  },
+  {
+    username: 'liker2',
+    profileImage: null,
+    followStatus: FOLLOW_STATUS.following,
+    myId: '1',
+    userId: '3',
+  },
+  {
+    username: 'liker3',
+    profileImage: 'https://picsum.photos/id/1003/1181/1772',
+    followStatus: null,
+    myId: '1',
+    userId: '4',
+  },
+  {
+    username: 'liker4',
+    profileImage: null,
+    followStatus: FOLLOW_STATUS.following,
+    myId: '1',
+    userId: '5',
+  },
+  {
+    username: 'liker5',
+    profileImage: null,
+    followStatus: FOLLOW_STATUS.following,
+    myId: '1',
+    userId: '6',
+  },
+  {
+    username: 'liker6',
+    profileImage: null,
+    followStatus: null,
+    myId: '1',
+    userId: '7',
+  },
+  {
+    username: 'liker7',
+    profileImage: null,
+    followStatus: FOLLOW_STATUS.following,
+    myId: '1',
+    userId: '8',
+  },
+  {
+    username: 'liker8',
+    profileImage: null,
+    followStatus: null,
+    myId: '1',
+    userId: '9',
+  },
+  {
+    username: 'liker9',
+    profileImage: null,
+    followStatus: null,
+    myId: '1',
+    userId: '10',
+  },
+];
+
+export default likers;
