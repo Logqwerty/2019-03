bf86fabb4a51a6057e9b3e565d1e180a4ef4f8e3
Logqwerty <inm2790@gmail.com>
2020-10-09 16:37:07 +0900

    [Feat] image URL fixtures 추가
    컴포넌트의 스토리 작성 시, 프로필이나 게시물의 테스트용 이미지 URL이 필요한
    경우가 잦아 image URL fixtures를 새로 추가하고 이에 따라 likers fixture에서
    하드코딩된 이미지 url를 새로 추가한 image url fixture로 변경.

---
+++ packages/web/src/__test__/fixtures/images.js
@@ -0,0 +1,35 @@
+const BASE_URL_PROFILES =
+  'https://youngstar-storage.s3.ap-northeast-2.amazonaws.com/profiles';
+
+const BASE_URL_POSTS =
+  'https://youngstar-storage.s3.ap-northeast-2.amazonaws.com/post';
+
+export const profileImages = [
+  `${BASE_URL_PROFILES}/profile_0000.jpg`,
+  `${BASE_URL_PROFILES}/profile_0001.jpg`,
+  `${BASE_URL_PROFILES}/profile_0002.jpg`,
+  `${BASE_URL_PROFILES}/profile_0003.jpg`,
+  `${BASE_URL_PROFILES}/profile_0004.jpg`,
+  `${BASE_URL_PROFILES}/profile_0005.jpg`,
+  `${BASE_URL_PROFILES}/profile_0006.jpg`,
+  `${BASE_URL_PROFILES}/profile_0007.jpg`,
+  `${BASE_URL_PROFILES}/profile_0008.jpg`,
+  `${BASE_URL_PROFILES}/profile_0009.jpg`,
+];
+
+export const profileImage = profileImages[0];
+
+export const postImages = [
+  `${BASE_URL_POSTS}/cat_0000.jpg`,
+  `${BASE_URL_POSTS}/cat_0001.jpg`,
+  `${BASE_URL_POSTS}/cat_0002.jpg`,
+  `${BASE_URL_POSTS}/cat_0003.jpg`,
+  `${BASE_URL_POSTS}/cat_0004.jpg`,
+  `${BASE_URL_POSTS}/cat_0005.jpg`,
+  `${BASE_URL_POSTS}/cat_0006.jpg`,
+  `${BASE_URL_POSTS}/cat_0007.jpg`,
+  `${BASE_URL_POSTS}/cat_0008.jpg`,
+  `${BASE_URL_POSTS}/cat_0009.jpg`,
+];
+
+export const postImage = postImages[0];
--- packages/web/src/__test__/fixtures/index.js
+++ packages/web/src/__test__/fixtures/index.js
@@ -1,1 +1,2 @@
 export * from './likers';
+export * from './images';
